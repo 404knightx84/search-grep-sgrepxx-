0333446d4454451b40a1cf721b2df33bda64c61b
Kalpesh Koli <183476871+404knightx84@users.noreply.github.com>
2026-08-30 08:31:11 +0530

    Implement Matcher constructor with pattern and case option

--- src/Matcher.cpp
+++ src/Matcher.cpp
@@ -1,1 +1,7 @@
+#include "Matcher.hpp"
+#include <cctype>
 
+Matcher::Matcher(std::string pattern, bool case_insensitive)
+    : pattern_(std::move(pattern)), case_insensitive_(case_insensitive) {
+    lps_ = build_lps();
+}
